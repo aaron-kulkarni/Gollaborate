d7e494c5d678226e4b465244f82d38b2a7b6b329
aaron-kulkarni <aaron.d.kulkarni@gmail.com>
2024-11-18 14:03:02 -0600

    Initial local tcp connection

---
+++ server/server.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"net"
+)
+
+func main() {
+
+	ln, err := net.Listen("tcp", ":4987")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+
+		go handleConnection(conn)
+	}
+}
+
+func handleConnection(conn net.Conn) {
+	// Close the connection when we're done
+	defer conn.Close()
+
+	// Read incoming data
+	buf := make([]byte, 1024)
+	_, err := conn.Read(buf)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// Print the incoming data
+	fmt.Printf("Received: %s", buf)
+}
