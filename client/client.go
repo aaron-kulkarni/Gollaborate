bdcb937e0d09c68149b238aaa1d4781dcb319b69
aaron-kulkarni <aaron.d.kulkarni@gmail.com>
2024-11-18 14:42:55 -0600

    testing non-local connection

--- client/client.go
+++ client/client.go
@@ -1,49 +1,89 @@
 package main
 
 import (
-	"bufio"
+	"context"
 	"fmt"
-	"net"
+	"log"
 	"os"
+
+	libp2p "github.com/libp2p/go-libp2p"
+	"github.com/libp2p/go-libp2p/core/network"
+	"github.com/libp2p/go-libp2p/core/peer"
+	"github.com/libp2p/go-libp2p/core/protocol"
+	"github.com/multiformats/go-multiaddr"
 )
 
+const protocolID = protocol.ID("/p2p/1.0.0")
+
 func main() {
-
-	if len(os.Args) == 1 {
-		fmt.Println("Please provide host:port to connect to")
-		os.Exit(1)
-	}
-
-	// Resolve the string address to a TCP address
-	tcpAddr, err := net.ResolveTCPAddr("tcp4", os.Args[1])
-
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-
-	// Connect to the address with tcp
-	conn, err := net.DialTCP("tcp", nil, tcpAddr)
-
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-
-	// Send a message to the server
-	_, err = conn.Write([]byte("Hello TCP Server\n"))
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-
-	// Read from the connection untill a new line is send
-	data, err := bufio.NewReader(conn).ReadString('\n')
-	if err != nil {
-		fmt.Println(err)
+	if len(os.Args) != 2 {
+		fmt.Println("Usage: go run client.go <main-node-multiaddr>")
 		return
 	}
 
-	// Print the data read from the connection to the terminal
-	fmt.Print("> ", string(data))
+	mainNodeAddr := os.Args[1]
+
+	ctx := context.Background()
+
+	// Create a new libp2p Host
+	// h, err := libp2p.New(ctx)
+	h, err := libp2p.New(
+		libp2p.NATPortMap(),                            // Enable NAT traversal
+		libp2p.ListenAddrStrings("/ip4/0.0.0.0/tcp/0"), // Listen on all interfaces and a random port
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Set a stream handler on the host
+	h.SetStreamHandler(protocolID, handleStream)
+
+	// Parse the main node's multiaddress
+	maddr, err := multiaddr.NewMultiaddr(mainNodeAddr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Extract the peer ID from the multiaddress
+	peerInfo, err := peer.AddrInfoFromP2pAddr(maddr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Connect to the main node
+	if err := h.Connect(ctx, *peerInfo); err != nil {
+		log.Fatal(err)
+	}
+
+	// Open a stream to the main node
+	s, err := h.NewStream(ctx, peerInfo.ID, protocolID)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer s.Close()
+
+	// Send a message to the main node
+	message := "Hello from peer!"
+	_, err = s.Write([]byte(message))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Message sent to main node:", message)
+
+	select {}
+}
+
+func handleStream(s network.Stream) {
+	log.Println("Got a new stream!")
+	defer s.Close()
+
+	buf := make([]byte, 1024)
+	n, err := s.Read(buf)
+	if err != nil {
+		log.Println("Error reading from stream:", err)
+		return
+	}
+
+	log.Printf("Received: %s\n", string(buf[:n]))
 }
--- server/server.go
+++ server/server.go
@@ -2,40 +2,71 @@ package main
 
 import (
 	"fmt"
-	"net"
+	"log"
+
+	libp2p "github.com/libp2p/go-libp2p"
+	"github.com/libp2p/go-libp2p/core/host"
+	"github.com/libp2p/go-libp2p/core/network"
+	"github.com/libp2p/go-libp2p/core/peer"
+	"github.com/libp2p/go-libp2p/core/peerstore"
+	"github.com/libp2p/go-libp2p/core/protocol"
+	"github.com/libp2p/go-libp2p/p2p/discovery/mdns"
 )
 
+const protocolID = protocol.ID("/p2p/1.0.0")
+const serviceTag = "p2p-discovery"
+
 func main() {
+	// ctx := context.Background()
 
-	ln, err := net.Listen("tcp", ":4987")
+	// Create a new libp2p Host
+	h, err := libp2p.New(
+			libp2p.NATPortMap(), // Enable NAT traversal
+			libp2p.ListenAddrStrings("/ip4/0.0.0.0/tcp/0"), // Listen on all interfaces and a random port
+		)
+
 	if err != nil {
-		fmt.Println(err)
-		return
+		log.Fatal(err)
 	}
 
-	for {
-		conn, err := ln.Accept()
-		if err != nil {
-			fmt.Println(err)
-			continue
-		}
+	// Set a stream handler on the host
+	h.SetStreamHandler(protocolID, handleStream)
 
-		go handleConnection(conn)
+	// Print the host's multiaddresses
+	fmt.Println("Main node is running. Connect to me at:")
+	for _, addr := range h.Addrs() {
+		fmt.Printf("%s/p2p/%s\n", addr, h.ID().String())
 	}
+
+	// Use mDNS for peer discovery
+	mdnsService := mdns.NewMdnsService(h, serviceTag, &discoveryNotifee{h: h})
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer mdnsService.Close()
+
+	select {}
 }
 
-func handleConnection(conn net.Conn) {
-	// Close the connection when we're done
-	defer conn.Close()
+func handleStream(s network.Stream) {
+	log.Println("Got a new stream!")
+	defer s.Close()
 
-	// Read incoming data
 	buf := make([]byte, 1024)
-	_, err := conn.Read(buf)
+	n, err := s.Read(buf)
 	if err != nil {
-		fmt.Println(err)
+		log.Println("Error reading from stream:", err)
 		return
 	}
 
-	// Print the incoming data
-	fmt.Printf("Received: %s", buf)
+	log.Printf("Received: %s\n", string(buf[:n]))
+}
+
+type discoveryNotifee struct {
+	h host.Host
+}
+
+func (n *discoveryNotifee) HandlePeerFound(pi peer.AddrInfo) {
+	log.Println("Discovered new peer:", pi.ID.String())
+	n.h.Peerstore().AddAddrs(pi.ID, pi.Addrs, peerstore.PermanentAddrTTL)
 }
