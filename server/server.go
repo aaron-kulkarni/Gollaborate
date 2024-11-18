3925bf5b80dfeeaf6cc88bc43b5ef6fac1fe62a0
aaron-kulkarni <aaron.d.kulkarni@gmail.com>
2024-11-18 17:08:53 -0600

    tcp connection working on local network

--- server/server.go
+++ server/server.go
@@ -17,14 +17,10 @@ const protocolID = protocol.ID("/p2p/1.0.0")
 const serviceTag = "p2p-discovery"
 
 func main() {
-	// ctx := context.Background()
-
 	// Create a new libp2p Host
 	h, err := libp2p.New(
-			libp2p.NATPortMap(), // Enable NAT traversal
-			libp2p.ListenAddrStrings("/ip4/0.0.0.0/tcp/0"), // Listen on all interfaces and a random port
-		)
-
+		libp2p.ListenAddrStrings("/ip4/0.0.0.0/tcp/0"), // Listen on all interfaces and a random port
+	)
 	if err != nil {
 		log.Fatal(err)
 	}
